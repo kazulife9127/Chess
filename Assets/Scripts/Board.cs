b1c6fe62e4c92670ce8e706f44f4a65c4c814fd2
春日和将 <k.k.9127.icpt2@gmail.com>
2022-06-14 16:26:00 +0900

    First Commit

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private Transform bottomLeftSquareTransform;
+    [SerializeField] private float squareSize;
+
+    public Vector3 CalculatePositionFromCoords(Vector2Int coords)
+    {
+        return bottomLeftSquareTransform.position + new Vector3(coords.x * squareSize, 0f, coords.y * squareSize);
+    }
+}
---
+++ Assets/Scripts/ChessGameController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PieceCreator))]
+public class ChessGameController : MonoBehaviour
+{
+    [SerializeField] private BoardLayout startingBoardLayout;
+    [SerializeField] private Board board;
+
+    private PieceCreator pieceCreator;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        SetDependencies();
+    }
+
+    private void SetDependencies()
+    {
+        pieceCreator = GetComponent<PieceCreator>();
+    }
+
+    private void Start()
+    {
+        StartNewGame();
+    }
+    private void StartNewGame()
+    {
+        CreatePiecesFromLayout(startingBoardLayout);
+    }
+
+    // 駒を生成する
+    private void CreatePiecesFromLayout(BoardLayout layout)
+    {
+        for (int i = 0; i < layout.GetPiecesCount(); i++)
+        {
+            Vector2Int squareCoords = layout.GetSquareCoordsAtIndex(i);
+            TeamColor team = layout.GetSquareTeamColorAtIndex(i);
+            string typeName = layout.GetSquarePieceNameAtIndex(i);
+
+            Type type = Type.GetType(typeName);
+            CreatePieceAndInitialize(squareCoords, team, type);
+        }
+    }
+
+    private void CreatePieceAndInitialize(Vector2Int squareCoords, TeamColor team, Type type)
+    {
+        Piece newPiece = pieceCreator.CreatePiece(type).GetComponent<Piece>();
+        newPiece.SetData(squareCoords, team, board);
+
+        Material teamMaterial = pieceCreator.GetTeamMaterial(team);
+        newPiece.SetMaterial(teamMaterial);
+    }
+}
