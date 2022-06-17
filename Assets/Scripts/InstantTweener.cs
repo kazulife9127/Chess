153f1c8b6e7db7156798b7030f49e2ddadccae01
春日和将 <k.k.9127.icpt2@gmail.com>
2022-06-17 16:24:13 +0900

    Add: Square Material

--- Assets/Scripts/InstantTweener.cs
+++ Assets/Scripts/InstantTweener.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class InstantTweener : MonoBehaviour, IObjectTweener
 {
-    
     public void MoveTo(Transform transform, Vector3 targetPosition)
     {
         transform.position = targetPosition;
